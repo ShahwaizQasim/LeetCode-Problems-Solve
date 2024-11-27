da1cca5e6bb80e1cf600482b63ae5d6965c8a6c0
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-11-27 00:16:47 -0800

    ispalimdromeproblem practice

--- 02.isPalindrome1.js
+++ 02.isPalindrome1.js
@@ -20,10 +20,15 @@ function isPalindrome1(s) {
     return result;
   }
 
+console.log(isPalindrome1("level"));
+
+
   /* practice */
-  // let test = "mad$##@@!!am";
-  // test = test.replaceAll(/[^a-z0-9]/g, "");
-  // console.log(test.split("").reverse().join(""));
+  let test = "mad$##@@!!am";
+  test = test.replaceAll(/[^a-z0-9]/g, "");
+  console.log(test.split("").reverse().join(""));
+// console.log(test); // madam
+
 
   let test2 = "Shahwaiz";
   console.log(test2.split(""));
@@ -31,18 +36,3 @@ function isPalindrome1(s) {
 
 //console.log(isPalindrome1("mad&%am")); //true palindrome
 // console.log(isPalindrome1("shahwaiz"));// false not palindrom
-
-  /* second method  */
-
-  function isPalindrome2(a){
-    a =  a.toLowerCase();
-    a = a.replaceAll(/[^a-z0-9]/g, "");
-    
-    for (let i = 0; i < a.length; i++) {
-       if (a[i] !== a[a.length - i]) {
-         return true;
-       }   
-    }
-    return false;
-  }
-  //console.log(isPalindrome2("A man, a plan, a canal: Panama"));
